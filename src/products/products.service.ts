e14d153f9a8622b937e8fc888454f2a9b931b1e5
Arash-IrDev <125612877+Arash-IrDev@users.noreply.github.com>
2023-05-27 17:57:40 +0300

    First Commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product, ProductSchema } from './schemas/product.schema';
+
+
+@Injectable()
+export class ProductsService {
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<ProductSchema>,
+  ) {}
+
+  async findAll(): Promise<Product[]> {
+    return this.productModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Product> {
+    return this.productModel.findById(id).exec();
+  }
+
+  async create(product: Product): Promise<Product> {
+    const newProduct = new this.productModel(product);
+    return newProduct.save();
+  }
+
+  async update(id: string, product: Product): Promise<Product> {
+    return this.productModel.findByIdAndUpdate(id, product, { new: true }).exec();
+  }
+
+  async delete(id: string): Promise<Product> {
+    return this.productModel.findByIdAndRemove(id).exec();
+  }
+}
